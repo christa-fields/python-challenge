53473e64a272f6f4b5e807f92f3701df9960e6f4
Christa-Fields <fields.christa@gmail.com>
2017-11-14 22:08:52 -0500

    push Bootcamp Python HW

--- pyparagraph/pyparagraph.py
+++ pyparagraph/pyparagraph.py
@@ -1,4 +1,5 @@
 #Import a text file filled with a paragraph of your choosing.
+import re 
 
 # Store the file path associated with the file 
 paragraph_1 = 'raw_data/paragraph_1.txt'
@@ -9,12 +10,15 @@ with open(paragraph_1, 'r') as text_1:
 	print(text_1)
 
 	# Store all of the text inside a variable called "lines"
+	#reades cvs file as string
 	lines_1 = text_1.read()
 	# Print the contents of the text file
 	print(lines_1)
 
 
 #Assess the passage for each of the following:
+	print ("Paragraph Analysis")
+	print("---------------------")
 
 	#Approximate word count
 	words = lines_1.split(" ")
@@ -22,15 +26,24 @@ with open(paragraph_1, 'r') as text_1:
 	print ("Approximate word count:") 
 	print(len(words))
 
-#Approximate sentence count
+	#Approximate sentence count
 	sentence = lines_1.split(".")
 
 	print ("Approximate sentence count:") 
 	print(len(sentence))
 	
 	#Approximate letter count (per word)
+	letter_count= sum(map(len, words))/ len(words)
+
 	print ("Approximate letter count (per word):") 
+	print(letter_count)
 
 	#Average sentence length (in words)
+	sentence_count= re.split(r"[/s]+", lines_1)
 	print ("Approximate sentence length (in words):") 
+	print(len(sentence_count))
+
+
+
+	
 
